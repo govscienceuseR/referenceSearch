2b1ff01ecc79443a0066bd1b5374dab0bdbcea0a
Tyler Scott <tascott@ucdavis.edu>
2023-01-13 09:54:34 -0700

    add treat as phrase options

--- R/search.R
+++ R/search.R
@@ -37,16 +37,17 @@ search_collection = function(query_string, collection_name, topn=1,conn=NULL) {
 #' @param citations dataframe of citations
 #' @param boost_fields optional fields to boost weights
 #' @param boost_values optional boost factors. Value is weight relative to other fields (e.g., ^2 is twice the weight) (see https://solr.apache.org/guide/7_3/the-standard-query-parser.html#boosting-a-term-with)
-
+#' @param treat_as_phrase character vector of variables to treat as phrase (i.e., in "quotes"), currently limited to title and journal title options
 #' @return search queries
 #' @export
-create_queries = function(citations,boost_values = NULL,boost_fields = NULL) {
+create_queries = function(citations,boost_values = NULL,boost_fields = NULL,treat_as_phrase = c('title','journal_title')) {
 
     # check input data
     valid = validate_columns(colnames(citations),
 		     expected=c("title", "authors", "year", "publisher", 
 				"doi", "journal_title"))
     if(!valid) { stop("columns don't match expected") }	
+    if(!any(treat_as_phrase) %in% c('journal_title','title')){stop(paste0("treating ", paste(treat_as_phrase[!treat_as_phrase %in% c('journal_title','title')],collapse = ',')," as phrase not currently supported"))}
     if(length(boost_fields)!=length(boost_values)|(length(boost_fields)>1&length(boost_values)==1)){stop("must specify a single boost value or 1 boost value for every field to boost")}
     if(!all(boost_fields %in% c("title", "authors", "year", "publisher", 
                                         "doi", "journal_title"))){stop("boost column doesn't match expected")}
@@ -63,15 +64,22 @@ create_queries = function(citations,boost_values = NULL,boost_fields = NULL) {
     citations$year = as.character(citations$year)
 
     # for title, add phrase
-    citations$title = sapply(citations$title, add_phrase, "title", 
-			     USE.NAMES = FALSE)
-    citations$title[citations$title == ""] = NA
-
+    if('title' %in% treat_as_phrase){
+      # for title, add phrase
+      citations$title = sapply(citations$title, add_phrase, "title", 
+                                       USE.NAMES = FALSE)
+      citations$title[citations$title == ""] = NA
+    }else{
+      citations$title = add_field(citations$title, "title")
+    }
+    if('journal_title' %in% treat_as_phrase){
     # for journal_title, add phrase
     citations$journal_title = sapply(citations$journal_title, add_phrase, "journal_title", 
                              USE.NAMES = FALSE)
     citations$journal_title[citations$journal_title == ""] = NA
-    
+    }else{
+      citations$journal_title = add_field(citations$journal_title, "journal_title")
+    }
     # for rest add field identifiers
     fn = c("authors", "year", "publisher", "doi")
     citations$authors = add_field(citations$authors, "authors")
